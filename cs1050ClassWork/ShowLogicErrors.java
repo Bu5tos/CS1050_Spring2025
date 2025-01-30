90b5fae135aa8e34e2db9833f12a84476ee91b42
Bu5tos <alexis.bustos2006@icloud.com>
2025-01-30 15:13:54 -0700

    Pair Programming
    Need to fix a logic error

---
+++ cs1050ClassWork/ShowLogicErrors.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ShowLogicErrors
+	{
+	public static void main(String[] args) {
+		final int TOTAL_QUIZZES = 3;
+		int quiz1 = 9;
+		int quiz2 = 10;
+		int quiz3 = 7;
+		double average = (quiz1 + quiz2 + quiz3)/TOTAL_QUIZZES;
+		System.out.println("average:" + average);
+	}
+
+}
