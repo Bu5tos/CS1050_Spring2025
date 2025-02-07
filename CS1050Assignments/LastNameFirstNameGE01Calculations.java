9a2f5536c35f1cce9c82fbcdd1e8c7311c89b3dc
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-07 01:36:07 -0700

    Update LastNameFirstNameGE01Calculation.java
    Need to do "Working with Expressions" next.

--- CS1050Assignments/LastNameFirstNameGE01Calculations.java
+++ CS1050Assignments/LastNameFirstNameGE01Calculations.java
@@ -1,18 +1,31 @@
-/**
- * 
+/*
+ *  Name: Alexis Bustos
+ *  Class: CS1050 (T/TH)
+ *  Due date: 02/09/25
+ *  Assignment 1: GE01 Ch01 Learning New Technologies, Set Up Environment, Version Control
+ *  Description: Guided Exploration 01 
+ *  This program will print a couple paragraphs about my passions and interests.       
+ *  The program will store and display my age and my aged squared. 
+ *  Optional: Ask for the user's age. Display if they are older, younger or same age.
  */
 
-/**
- * 
- */
-public class LastNameFirstNameGE01Calculations {
 
-	/**
-	 * @param args
-	 */
+public class LastNameFirstNameGE01Calculations 
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		// First paragraph
+        System.out.println("Hello, I am Alexis! I am passionate about listening to all kinds of music, they're everything to me and I can't live without them.");
+        System.out.println("I love to lift weights at a gym while listening to hardcore music to stay motivated and fired up!");
+        System.out.println("I enjoy watching anime here and there. Especially romance and thriller because I like to see something happy and lovely, and mysterious or action on another genre.");
+        System.out.println("Furthermore, I am a gamer who loves and enjoys to play singular player games because it is fascinating and emotional to play story games.");
+        System.out.println("");
+        
+        // Second paragraph
+        System.out.println("I am working towrd a bachelor's degree of Ccomputer science which is challenging but content about this career.");
+        System.out.println("I am a first generation stident. Currently, my biggest goal is to graduate as a college student and inspire future generations in my community.");
+        System.out.println("I work at Chipotle as a crew member. I'll admit that I am a hard working individual and plan to do the same in college to achieve my goals.");
+        System.out.println("Lastly, I am an introvert who likes to listen to others and an amicable human being.");
+	
+	}	
 
 }
