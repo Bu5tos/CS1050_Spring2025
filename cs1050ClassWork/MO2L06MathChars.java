324454c6e28f035ac90844beb327da011d75167d
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-11 13:57:53 -0700

    M02L06MathChars.java
    needs fixing.

---
+++ cs1050ClassWork/MO2L06MathChars.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+public class MO2L06MathChars
+	{
+	
+	public static void main(String[] args) 
+	{
+
+		double exponentResult = Math.pow(10, 2);
+		System.out.println("result =" + exponentResult);
+		System.out.println("");
+		
+	    double randomDouble = Math.random();
+	    int randomInt = (int)randomDouble;
+	    randomInt = (int)(randomDouble * 10);
+	    randomInt = (int)(randomDouble * 10 + 1);
+	
+   	    //Note: ('') = character. ("") = string
+        char middleInitial = '1';//Assigns the character 4 to numCharacter
+        int charIntValue = 1;
+   
+        System.out.println(middleInitial);
+        charIntValue = (int)middleInitial;
+        System.out.println("");
+        
+        System.out.println(charIntValue);
+	
+	    Scanner input = new Scanner(System.in);
+	
+	    System.out.println("Enter a char");
+	    char someChar = input.next().charAt(0);
+	    System.out.printf("A char was enetred with ascii value %d\n", someChar);
+	
+	    String firstName = "Bu5tos";
+	
+	}
+
+}
