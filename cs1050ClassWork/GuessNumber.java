af3497bf0d8fcdaff4fe985f4d956830110c32cd
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-15 20:46:15 -0700

    Update GuessNumber.java

--- cs1050ClassWork/GuessNumber.java
+++ cs1050ClassWork/GuessNumber.java
@@ -7,7 +7,7 @@
 import java.util.Scanner; //Import Scanner class
 
 public class GuessNumber //Class Identifier: "GuessNumber"
-{
+{ // The start of program
 
 	/**
 	 * @param args
@@ -27,19 +27,24 @@ public class GuessNumber //Class Identifier: "GuessNumber"
 	    // If guess doesn't match to number, the code will run repeatedly until the number is correct
 	    while (guess != number)
 	    {
-	      //
+	      // "\n" makes the text move to the next line
 	      System.out.print("\nEnter your guess: ");
 	      if (input.hasNextInt())
 	      {
 	      guess = input.nextInt();
-
+          // If user's int equals to random generated number
 	      if (guess == number)
-	        System.out.println("Yes, the number is " + number);
+	        //prints the statement and random number
+	    	System.out.println("Yes, the number is " + number);
+	        // If user's number is greater than number, it will be too high
 	      else if (guess > number)
 	        System.out.println("Your guess is too high");
+	        // If it's less than random number, it will be too low
 	      else
 	        System.out.println("Your guess is too low");
-	      } else 
+	      } 
+	        // This will display if User's guess isn't an integer
+	      else 
 	      {
 	    	  System.out.println("Invalid input. Please enter an integer.");
 	    	  input.next();
@@ -49,4 +54,4 @@ public class GuessNumber //Class Identifier: "GuessNumber"
 		System.out.println("Thank you for playing!");
 	}
 
-}
+} // The end of program
