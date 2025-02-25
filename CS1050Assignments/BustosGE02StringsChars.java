d5815d74969527a21d826f060442d195083adaad
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-25 11:50:07 -0700

    update BustosGE02StringsChars.java

--- CS1050Assignments/BustosGE02StringsChars.java
+++ CS1050Assignments/BustosGE02StringsChars.java
@@ -8,25 +8,36 @@
 
 import java.util.Scanner;
 
-public class BustosGE02StringsChars {
+public class BustosGE02StringsChars 
+{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter student 1 first name: " );
+		String firstName1 = input.next();
+		System.out.println("Enter student 1 last name: " );
+		String lastName1 = input.next();
+		System.out.println("Enter student 2 first name: " );
+		String firstName2 = input.next();
+		System.out.println("Enter student 2 last name: " );
+		String lastName2 = input.next();
+		
+		System.out.println("Students in alphabetical order by last name:" );
+		
+	    if (lastName1.compareToIgnoreCase(lastName2) > 0)
+	    {
+	    	System.out.print(firstName2 + " " + lastName2);
+	    }
+	    else
+	    {
+	    	System.out.println(firstName1 + " " + lastName1);
+	    }
 	}
-
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter student 1 first name: " );
-	String firstName1 = input.next();
-	System.out.println("Enter student 1 last name: " );
-	String lastName1 = input.next();
-	System.out.println("Enter student 2 first name: " );
-	String firstName2 = input.next();
-	System.out.println("Enter student 2 last name: " );
-	String lastName2 = input.next();
-	
-	System.out.println("Students in alphabetical order by last name:" );
 }
+
