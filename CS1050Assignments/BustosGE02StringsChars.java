ad418307a9a3bb98defd5b62d2e086b4779b4692
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-24 20:32:41 -0700

    Update BustosGE02StringsChars.java

--- CS1050Assignments/BustosGE02StringsChars.java
+++ CS1050Assignments/BustosGE02StringsChars.java
@@ -5,9 +5,6 @@
 /**
  * 
  */
-
-import java.util.Scanner;
-
 public class BustosGE02StringsChars {
 
 	/**
@@ -15,22 +12,19 @@ public class BustosGE02StringsChars {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter student 1 first name: " );
-		String firstName1 = input.next();
-		System.out.println("Enter student 1 last name: " );
-		String lastName1 = input.next();
-		System.out.println("Enter student 2 first name: " );
-		String firstName2 = input.next();
-		System.out.println("Enter student 2 last name: " );
-		String lastName2 = input.next();
-		
-		System.out.println("Students in alphabetical order by last name:" );
-		
-		
-		
-		
+
 	}
 
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter student 1 first name: " );
+	String firstName1 = input.next();
+	System.out.println("Enter student 1 last name: " );
+	String lastName1 = input.next();
+	System.out.println("Enter student 2 first name: " );
+	String firstName2 = input.next();
+	System.out.println("Enter student 2 last name: " );
+	String lastName2 = input.next();
+	
+	System.out.println("Students in alphabetical order by last name:" );
+	
 }
