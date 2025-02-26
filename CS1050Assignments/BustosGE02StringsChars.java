328ea05af8c6716870bac11fab3beed94954bea6
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-25 17:04:25 -0700

    Update BustosGE02StringsChars.java
    Needs fixing on char

--- CS1050Assignments/BustosGE02StringsChars.java
+++ CS1050Assignments/BustosGE02StringsChars.java
@@ -18,6 +18,7 @@ public class BustosGE02StringsChars
 	{
 		// TODO Auto-generated method stub
 
+		//User types in first and last name twice 
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter student 1 first name: " );
 		String firstName1 = input.next();
@@ -30,14 +31,30 @@ public class BustosGE02StringsChars
 		
 		System.out.println("Students in alphabetical order by last name:" );
 		
+		//Comparing last name with first name
 	    if (lastName1.compareToIgnoreCase(lastName2) > 0)
 	    {
 	    	System.out.print(firstName2 + " " + lastName2);
+	    	System.out.println(firstName1 + " " + lastName1);
+	    	
 	    }
-	    else
+	    else if (lastName1.compareToIgnoreCase(lastName2) < 0)
 	    {
 	    	System.out.println(firstName1 + " " + lastName1);
+	    	System.out.print(firstName2 + " " + lastName2);
 	    }
+	
+	    System.out.println(" ");
+	    System.out.println("Initials: ");
+	    
+	    char firstInitial1 = firstName1.charAt(0);
+	    char lastInitial1 = lastName1.charAt(0);
+	    char firstInitial2 = firstName2.charAt(0);
+	    char lastInitial2 = lastName2.charAt(0);
+	    
+        System.out.println("Student 1: " + firstInitial1 + ". " + lastInitial1 + ".");
+        System.out.println("Student 2: " + firstInitial2 + ". " + lastInitial2 + ".");
+	    
 	}
 }
 
