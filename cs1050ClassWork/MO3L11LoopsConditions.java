445594ea8bda66f51b3bc6eb46b639e2292c8c45
Bu5tos <alexis.bustos2006@icloud.com>
2025-02-27 13:59:43 -0700

    MO3L11LoopsConditions.java

---
+++ cs1050ClassWork/MO3L11LoopsConditions.java
@@ -0,0 +1,18 @@
+
+public class MO3L11LoopsConditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int total = 0;
+	int counter = 0;
+	
+	for (int sum = 1; sum <= 5; sum++)
+	{
+		counter = counter + 1;
+		total = total + counter;
+		System.out.println("Counter: " + counter);
+		System.out.println("Total: " + total);
+	}
+	}
+}
