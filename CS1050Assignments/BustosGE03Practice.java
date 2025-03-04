fe59ed933d9945f0be9b825fe0129f812ae6e4c5
Bu5tos <alexis.bustos2006@icloud.com>
2025-03-04 16:02:23 -0700

    BustosGE03Practice.java

---
+++ CS1050Assignments/BustosGE03Practice.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class BustosGE03Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
