7653887d1034a843dcb5daf121df18e00a961723
Bu5tos <alexis.bustos2006@icloud.com>
2025-03-04 19:55:33 -0700

    Update BustosGE03Practice.java

--- CS1050Assignments/BustosGE03Practice.java
+++ CS1050Assignments/BustosGE03Practice.java
@@ -1,18 +1,18 @@
-/**
- * 
- */
-
-/**
- * 
- */
 public class BustosGE03Practice {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int counter = 0;
+		int total = 0;
+		int sum = 0;
+		
+		while (sum <= 6) {
+			counter = counter + 1;
+			total = total + counter;
+			sum++;
+		}
+		
 	}
 
 }
