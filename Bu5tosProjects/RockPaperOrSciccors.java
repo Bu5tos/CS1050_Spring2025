9394f07d09530dc2b36ed6403fd1b99c3a823e78
Bu5tos <alexis.bustos2006@icloud.com>
2025-03-12 00:02:35 -0600

    Changes

--- Bu5tosProjects/RockPaperOrSciccors.java
+++ Bu5tosProjects/RockPaperOrSciccors.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+
 
 public class RockPaperOrSciccors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
 		
-        System.out.println("Insert name here, player one: ");
-		String playerOne = input.next();
-		System.out.println("Insert name here, player two: ");
-		String playerTwo = input.next();
+	
+		int oddNumber = 0;
+		int oddCounter = 0;
+		int max = 7;
 		
-		System.out.println("Choose VERY carefully, " + playerOne + ". Rock(R), paper(P), or scissors(S): ");
-		char category = input.next().charAt(0);
+		for (int sum = 0; sum <= max; sum++) {
+			
+			oddCounter = oddCounter + 1;
+			oddNumber = oddNumber + oddCounter;
+			
+			System.out.println("Total odd number: " + oddNumber);
+		
 	}
-
+  }
 }
