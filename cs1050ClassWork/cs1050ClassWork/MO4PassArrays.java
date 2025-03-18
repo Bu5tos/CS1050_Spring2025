774854c16c60b2734292814271f1bc24c1584ca7
Bu5tos <alexis.bustos2006@icloud.com>
2025-03-18 13:49:06 -0600

    Tech doc, testpass array

---
+++ cs1050ClassWork/cs1050ClassWork/MO4PassArrays.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cs1050ClassWork;
+
+/**
+ * 
+ */
+public class MO4PassArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
