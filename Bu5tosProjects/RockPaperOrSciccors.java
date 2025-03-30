30f239882558ddb91f782c1cecebaf650832e3bc
Bu5tos <alexis.bustos2006@icloud.com>
2025-03-29 18:35:57 -0600

    Update RockPaperOrSciccors.java

--- Bu5tosProjects/RockPaperOrSciccors.java
+++ Bu5tosProjects/RockPaperOrSciccors.java
@@ -5,16 +5,7 @@ public class RockPaperOrSciccors {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	int oddNumbers = 0;
-	int counter = 0;
-	
-	for (int sum = 0; sum < 5; sum++) {
-	
-		counter = counter + 1;
-		oddNumbers = oddNumbers + counter;
 		
-		System.out.println("Total odd number is: " + (oddNumbers / 2));
-		
-	}
+	
   }
 }
