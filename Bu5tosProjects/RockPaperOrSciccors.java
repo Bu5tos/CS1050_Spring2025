f3cae45fdedac8916554475a7269d5609862b37e
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-06 14:23:00 -0600

    RockPaperOrSciccors.java

--- Bu5tosProjects/RockPaperOrSciccors.java
+++ Bu5tosProjects/RockPaperOrSciccors.java
@@ -5,14 +5,6 @@ public class RockPaperOrSciccors {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		int underweight = 51;
-		
-		if (underweight <= 50) {
-			System.out.println("Athlete is " + underweight);
-		}
-		else {
-			System.out.println("Athlete is not" + underweight);
-		}
+
   }
 }
