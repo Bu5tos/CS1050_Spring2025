ec77300389b2ac6cb7ec371e51da9fb7af293efa
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-08 17:40:10 -0600

    UnitTestGym.java

---
+++ cs1050ClassWork/cs1050ClassWork/UnitTestGym.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cs1050ClassWork;
+
+/**
+ * 
+ */
+public class UnitTestGym {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
