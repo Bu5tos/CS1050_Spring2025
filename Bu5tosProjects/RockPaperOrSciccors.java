e94d7c39d116b6d31268426ce617c69853f3d858
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-09 22:59:57 -0600

    RockPaperOrSciccors.java
    Finally did something..

--- Bu5tosProjects/RockPaperOrSciccors.java
+++ Bu5tosProjects/RockPaperOrSciccors.java
@@ -1,11 +1,24 @@
-
+import java.util.Scanner;
 
 public class RockPaperOrSciccors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		char[] rockPaperOrScissors = {'R', 'P', 'S'};
 		
+		System.out.println("Welcome to rock, paper, or scissors!");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter name: ");
+		String player = input.next();
+		System.out.println("Hello " + player + ", you will be up against the computer!");
+		System.out.println("Enter R, P, or S (for short): ");
 
   }
+	
+	public static void computerOpponent(char rockPaperScissors) {
+	
+		
+		
+}
 }
