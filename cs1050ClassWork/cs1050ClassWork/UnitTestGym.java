7408febc6bfec008d435a0a79d479a78f7a38e15
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-09 20:27:59 -0600

    Update UnitTestGym.java
    Fixed it

--- cs1050ClassWork/cs1050ClassWork/UnitTestGym.java
+++ cs1050ClassWork/cs1050ClassWork/UnitTestGym.java
@@ -14,6 +14,22 @@ public class UnitTestGym {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	int[] arrayCalories = {100, 200, 400, 350, 273};
+	
+	int topAthlete = getTopAthlete(arrayCalories);
+	System.out.println("Top athlete is " + topAthlete);
+	
+	}
+		
+	public static int getTopAthlete(int[] arrayCalories) {
+	
+	int largest = arrayCalories[0];
+	
+	for (int i = 1; i < arrayCalories.length; i++) {	
+		if (arrayCalories[i] > largest)
+		   largest = arrayCalories[i];	   
+	}
+	    return largest;
 	}
 
 }
