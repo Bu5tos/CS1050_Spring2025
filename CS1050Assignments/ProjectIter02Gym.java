3f0126e91728f6d9ec62564d862ea8b3baca3275
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-29 16:35:27 -0600

    Update ProjectIter02Gym.java

--- CS1050Assignments/ProjectIter02Gym.java
+++ CS1050Assignments/ProjectIter02Gym.java
@@ -79,7 +79,12 @@ class Athlete {
 	private int[] dailyCaloriesBurned;
 	
 	public Athlete(String firstName, String lastName, double weight, double height, int age, int[] dailyCaloriesBurned) {
-		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.weight = weight;
+		this.height = height;
+		this.age = age;
+		this.dailyCaloriesBurned = dailyCaloriesBurned;
 	}
 	
 	public String getFirstName() {
@@ -90,22 +95,52 @@ class Athlete {
 		
 	}
 	
-	public double getAverageCaloriesBurned() {
+	public double getAverageCaloriesBurned(double dailyBurnedCalories[]) {
+		double totalCalories = 0;
+		double sum = 0;
 		
+		//Sum daily burned calories 
+		for (int index = 0; index < dailyBurnedCalories.length; index++) {
+			sum = 0 + dailyBurnedCalories[index];
+			totalCalories = totalCalories + sum;
+		}
+		return totalCalories/7;
 	}
 	
-	public int calculateMaxHeartRate() {
-		
+	public int calculateMaxHeartRate(int age) {
+		final int MAX_HEART_RATE = 220; //Constant max heart rate
+		int bpm = (MAX_HEART_RATE - age);
+		return bpm;
 	}
 	
 	public double calculateBMI() {
 		
 	}
 	
-	public String getBMICategory() {
+	public String getBMICategory(double bmi) {
+		final double MAX_UNDER_WEIGHT = 18.4;
+		final double MIN_NORMAL = 18.5;
+		final double MIN_OVER_WEIGHT = 25.0;
+		final double MIN_OBESE = 40.0;
 		
+		String category = " ";
+		
+		//Determine BMI category
+		if (bmi <= MAX_UNDER_WEIGHT){
+			    category = "Underweight";
+			}
+			else if (bmi >= MIN_OBESE ){
+				category = "Obese";
+			}
+			else if (bmi >= MIN_OVER_WEIGHT){
+				category = "Overweight";
+			}
+			else if (bmi >= MIN_NORMAL){
+				category = "Normal";
+			}
+		return category;
 	}
 	
-}
+}//End class Athlete
 
 // Gym Class
