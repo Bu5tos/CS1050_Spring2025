620fa7c7f3b4445c087b376bccb3eca45da4bbc2
Bu5tos <alexis.bustos2006@icloud.com>
2025-04-29 13:49:07 -0600

    Update ProjectIteration02Gym.java

---
+++ Bu5tosProjects/ProjectIteration02Gym.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ProjectIteration02Gym {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
