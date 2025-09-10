e3ee1a4ceb65a60f38dbf7ec396125d18f4ed076
Bu5tos <alexis.bustos2006@icloud.com>
2025-09-09 21:59:27 -0600

    Some notes

--- cs1050ClassWork/LibraryAppTest.java
+++ cs1050ClassWork/LibraryAppTest.java
@@ -2,7 +2,52 @@ public class LibraryAppTest {
 
 	public static void main(String[] args) {
 
-	
+		// --- unit test checks for Book ---
+		System.out.println("Unit Test Book Class");
+		Book unitTestBook = new Book("Unmasking AI", "Joy Buolamwini", 2023);
+		System.out.println("getTitle():   " + unitTestBook.getTitle());
+		System.out.println("getAuthor():  " + unitTestBook.getAuthor());
+		System.out.println("getYear():    " + unitTestBook.getYear());
+		System.out.println("stringOfBookDetails():   " + unitTestBook.stringOfBookDetails());
+		System.out.println();
+		System.out.println("Setting up Test Library");
+		int numberOfShelves = 3;
+		int shelfCapacity = 4;
+		System.out.println("Shelves (rows): " + numberOfShelves);
+		System.out.println("Slots per shelf (columns): " + shelfCapacity);
+		System.out.println("Total capacity: " + (numberOfShelves * shelfCapacity));
+		System.out.println();
+		Library library = new Library("Test Library", numberOfShelves, shelfCapacity);
+		library.displayCountPerShelf();
+		library.printAllBooks();
+		library.displayOldest();
+		// Row 0
+		library.addBook(null);
+		library.addBook(new Book("Unmasking AI", "Joy Buolamwini", 2023));
+		library.addBook(new Book("Hello World", "Hannah Fry", 2018));
+		library.addBook(new Book("Race After Technology", "Ruha Benjamin", 2019));
+		library.addBook(new Book("Deep Learning", "Ian Goodfellow", 2016));
+		library.displayCountPerShelf();
+		library.printAllBooks();
+		library.displayOldest();
+		// Row 1
+		library.addBook(new Book("Algorithms to Live By", "Brian Christian", 2016));
+		library.addBook(new Book("Weapons of Math Destruction", "Cathy O'Neil", 2016));
+		library.addBook(new Book("The Mythical Man-Month", "Fred Brooks", 1975));
+		library.addBook(new Book("Refactoring", "Martin Fowler", 1999));
+		// Row 2
+		library.addBook(new Book("The Pragmatic Programmer", "Andrew Hunt & David Thomas", 1999));
+		library.addBook(new Book("Peopleware", "Tom DeMarco & Tim Lister", 1987));
+		library.addBook(new Book("Computer Lib / Dream Machines", "Ted Nelson", 1975));
+		library.displayCountPerShelf();
+		library.printAllBooks();
+		library.displayOldest();
+		System.out.println();
+		System.out.println("Test add more books than capacity...");
+		library.addBook(new Book("Extra Title", "Extra Author", 2024)); // should trigger "full" message
+		library.displayCountPerShelf();
+		library.printAllBooks();
+		library.displayOldest();
 		
 	}//End main
 	
@@ -70,11 +115,14 @@ class Library {
 	}
 	
 	public boolean addBook(Book book) {
+		
+		//Return false if book is null
 		if (book == null) {
 			System.out.println("No Book");
 			return false;
 		}
 		
+		//Can't add certain book if library is full
 		if (isFull) {
 			System.out.println("Library is full, can't add " + book.stringOfBookDetails());
 			return false;
@@ -89,6 +137,8 @@ class Library {
 	}
 	
 	public void printALlBooks() {
+		
+		//For each slot it would display book details
 		for (int slot = 0; slot < bookShelf; slot++) {
 			
 				Book bookShelf = shelfCapacity[slot];
@@ -100,6 +150,8 @@ class Library {
 		}
 	
 	public void oldestBook() {
+		
+		//get details of book and lowest year will be declared
 		int oldestYear = bookShelf[0].getYear();
 		int oldestBook = 0;
 		
