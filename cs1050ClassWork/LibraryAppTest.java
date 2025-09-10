0b4137f61edf0283ab84eea99ba6757e88080d78
Bu5tos <alexis.bustos2006@icloud.com>
2025-09-09 21:47:08 -0600

    Updates

--- cs1050ClassWork/LibraryAppTest.java
+++ cs1050ClassWork/LibraryAppTest.java
@@ -2,53 +2,7 @@ public class LibraryAppTest {
 
 	public static void main(String[] args) {
 
-		// --- unit test checks for Book ---
-		System.out.println("Unit Test Book Class");
-		Book unitTestBook = new Book("Unmasking AI", "Joy Buolamwini", 2023);
-		System.out.println("getTitle():   " + unitTestBook.getTitle());
-		System.out.println("getAuthor():  " + unitTestBook.getAuthor());
-		System.out.println("getYear():    " + unitTestBook.getYear());
-		System.out.println("stringOfBookDetails():   " + unitTestBook.stringOfBookDetails());
-		System.out.println();
-		System.out.println("Setting up Test Library");
-		int numberOfShelves = 3;
-		int shelfCapacity = 4;
-		System.out.println("Shelves (rows): " + numberOfShelves);
-		System.out.println("Slots per shelf (columns): " + shelfCapacity);
-		System.out.println("Total capacity: " + (numberOfShelves * shelfCapacity));
-		System.out.println();
-		Library library = new Library("Test Library", numberOfShelves, shelfCapacity);
-		library.displayCountPerShelf();
-		library.printAllBooks();
-		library.displayOldest();
-		// Row 0
-		library.addBook(null);
-		library.addBook(new Book("Unmasking AI", "Joy Buolamwini", 2023));
-		library.addBook(new Book("Hello World", "Hannah Fry", 2018));
-		library.addBook(new Book("Race After Technology", "Ruha Benjamin", 2019));
-		library.addBook(new Book("Deep Learning", "Ian Goodfellow", 2016));
-		library.displayCountPerShelf();
-		library.printAllBooks();
-		library.displayOldest();
-		// Row 1
-		library.addBook(new Book("Algorithms to Live By", "Brian Christian", 2016));
-		library.addBook(new Book("Weapons of Math Destruction", "Cathy O'Neil", 2016));
-		library.addBook(new Book("The Mythical Man-Month", "Fred Brooks", 1975));
-		library.addBook(new Book("Refactoring", "Martin Fowler", 1999));
-		// Row 2
-		library.addBook(new Book("The Pragmatic Programmer", "Andrew Hunt & David Thomas", 1999));
-		library.addBook(new Book("Peopleware", "Tom DeMarco & Tim Lister", 1987));
-		library.addBook(new Book("Computer Lib / Dream Machines", "Ted Nelson", 1975));
-		library.displayCountPerShelf();
-		library.printAllBooks();
-		library.displayOldest();
-		System.out.println();
-		System.out.println("Test add more books than capacity...");
-		library.addBook(new Book("Extra Title", "Extra Author", 2024)); // should trigger "full" message
-		library.displayCountPerShelf();
-		library.printAllBooks();
-		library.displayOldest();
-
+	
 		
 	}//End main
 	
@@ -115,32 +69,59 @@ class Library {
 		return name;
 	}
 	
-	public boolean addBook(Book bookShelf) {
+	public boolean addBook(Book book) {
+		if (book == null) {
+			System.out.println("No Book");
+			return false;
+		}
+		
+		if (isFull) {
+			System.out.println("Library is full, can't add " + book.stringOfBookDetails());
+			return false;
+		}
+		
 		for (int i = 0; i < shelfCapacity; i++) {
 			if (shelfCapacity > shelfCapacity) {
 				System.out.println(isFull);
 			}
-			
-			
+					
 		}
 	}
 	
 	public void printALlBooks() {
-		for (int slot = 0; slot < shelfCapacity; slot++) {
-			if (shelfCapacity == null) {
-				return false;
+		for (int slot = 0; slot < bookShelf; slot++) {
+			
+				Book bookShelf = shelfCapacity[slot];
+				
+				System.out.println(bookShelf.getAuthor());
+				System.out.println(bookShelf.getTitle());
+				System.out.println(bookShelf.getYear());
 			}
-			
-			Book bookShelf = shelfCapacity[slot];
-			
-			System.out.println()
 		}
-		
-	}
 	
 	public void oldestBook() {
-		int oldestYear = bookShelf[0].getYear;
+		int oldestYear = bookShelf[0].getYear();
+		int oldestBook = 0;
+		
+		for (int i = 1; i < shelfCapacity; i++) {
+			if (bookShelf.getYear() > oldestYear) {
+				oldestYear = bookShelf[i].getYear();
+				oldestBook = i;
+			}
+		}
 	}
+	
+	public int countPerShelf() {
+		for(int row = 0; row < shelf.length; row++) {
+			int sum = 0;
+		}
+		
+		for (int col 0; col < shelfCapacity[row].length; col++) {
+			sum += shelfCapacity[row][col];
+		}
+	}
+	
+	
 	
 }//End Library class
 
