494d50065c2939475b90a6f564d0f70ae2d6ed7f
Bu5tos <alexis.bustos2006@icloud.com>
2025-09-11 15:06:45 -0600

    Comments

--- CS2050M02/SelectionSortTDD.java
+++ CS2050M02/SelectionSortTDD.java
@@ -5,7 +5,7 @@ public class SelectionSortTDD
 		System.out.println("Testing Selection Sort\n");
 
 		int[][] testCases = { 
-				{ 4, 2, 7, 1, 5 }, // Regular case
+				{ 72, 50, 10, 44, 8, 20 }, // Regular case
 				{}, // Empty array
 				{ 5 }, // Single element
 				{ 1, 2, 3, 4, 5 }, // Already sorted
@@ -27,16 +27,17 @@ public class SelectionSortTDD
 
 	}
 
-
-
+	//Finding shortest number
 	public static void selectionSort(int[] array)
 	{
 		int shortestNumber = array[0];
+		//Checks if i is less than array indexes
 		for (int i = 1; i < array.length; i++) {
+			//if index value is < CURRENT value
 			if (array[i] < shortestNumber) {
+				//Initialize new shortest value
 				shortestNumber = array[i];
 			}
-			
 		}
 		System.out.println("Shortest number is " + shortestNumber);
 	}
