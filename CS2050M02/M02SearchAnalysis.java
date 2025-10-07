bbc6c865f589762a545ed698294e4de90af95731
Bu5tos <alexis.bustos2006@icloud.com>
2025-10-07 13:53:47 -0600

    SearchAnalysis class

--- CS2050M02/M02SearchAnalysis.java
+++ CS2050M02/M02SearchAnalysis.java
@@ -1,4 +1,4 @@
-/**
+/*/**
  * 
  */
 
@@ -11,7 +11,12 @@ public class M02SearchAnalysis
 		int[] sizes =
 		{ 10, 100, 1000, 10000 };
 
-		for (int n : sizes)
+		/**
+		 * Enhanced for loop (for-each loop)
+		 * 
+		 *
+		 */
+		for (int n : sizes) 
 		{
 			// Build a sorted array [1, 2, 3, ..., n]
 			int[] data = new int[n];
@@ -35,60 +40,20 @@ public class M02SearchAnalysis
 			linearSearch(data, lastKey);
 			linearSearch(data, absentKey);
 
-			// Run Binary Search
-			System.out.println("-- Binary Search --");
-			binarySearch(data, firstKey);
-			binarySearch(data, middleKey);
-			binarySearch(data, lastKey);
-			binarySearch(data, absentKey);
 		}
 	}
 
 	// ---------- Linear Search with comparison counting ----------
 	public static int linearSearch(int[] data, int key)
 	{
-		int comparisons = 0;
-		for (int i = 0; i < data.length; i++)
-		{
-			comparisons++;
-			if (data[i] == key)
-			{
-				System.out.println(
-						"Linear: key " + key + " found at index " + i + " after " + comparisons + " comparisons.");
-				return i;
-			}
-		}
-		System.out.println("Linear: key " + key + " not found after " + comparisons + " comparisons.");
+
 		return -1;
 	}
 
 	// ---------- Binary Search with comparison counting ----------
 	public static int binarySearch(int[] data, int key)
 	{
-		int low = 0;
-		int high = data.length - 1;
-		int comparisons = 0;
 
-		while (low <= high)
-		{
-			int mid = (low + high) / 2;
-			comparisons++;
-			if (data[mid] == key)
-			{
-				System.out.println(
-						"Binary: key " + key + " found at index " + mid + " after " + comparisons + " comparisons.");
-				return mid;
-			} else if (data[mid] < key)
-			{
-				comparisons++;
-				low = mid + 1;
-			} else
-			{
-				comparisons++;
-				high = mid - 1;
-			}
-		}
-		System.out.println("Binary: key " + key + " not found after " + comparisons + " comparisons.");
 		return -1;
 	}
 
