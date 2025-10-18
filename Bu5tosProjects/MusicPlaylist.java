a6141b28561d99509dd6c1f11b6f37c39651734a
Bu5tos <alexis.bustos2006@icloud.com>
2025-10-18 14:32:03 -0600

    Song class completed

---
+++ Bu5tosProjects/MusicPlaylist.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+
+public class MusicPlaylist {
+
+	public static void main(String[] args) {
+		
+		//Unit testing song info
+		Song unitTestSong = new Song("Joji", "Pixelated Kisses", 109);
+		System.out.println(unitTestSong.toString());
+		
+	}//End main
+	
+}//End class
+
+class Song {
+	
+	/**
+	 * Encapsulating variables to be private
+	 * Provide getters and setters to access or modify these instances
+	 */
+	private String artist;
+	private String title;
+	private int songLength;
+	
+	/**
+	 * Constructor:
+	 * A new object is created by this special method
+	 * "this" keyword gives the current object's variables
+	 * Initialize object's instances with given values 
+	 */
+	public Song(String artist, String title, int songLength) {
+		
+		this.artist = artist;
+		this.title = title;
+		this.songLength = songLength;
+	}
+	
+	//Methods
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setSongLength(int songLength) {
+		this.songLength = songLength;
+	}
+	
+	public int getSongLength() {
+		return songLength;
+	}
+	
+	
+	//returns a string that describes the Song object
+	@Override // Overrides Playlist Class method to it's own version(behavior)
+	public String toString() {
+		
+		//Prints artist, title, songLength together as a string
+		return artist + ", "+ title + ", " + songLength + " Seconds";
+	}
+	
+}//End Song class
